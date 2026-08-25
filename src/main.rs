4601c5243ade672c1acd7211d1113352bf72aff9
mhieuhonda <mhieuhonda@users.noreply.github.com>
2026-08-25 23:58:50 +0000

    fix(clippy): áp dụng clippy --fix suggestions trong src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt()
         .with_env_filter(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "khogame=debug,tower_http=info".into()),
+                .unwrap_or_else(|_| return "khogame=debug,tower_http=info".into()),
         )
         .with_target(true)
         .init();
